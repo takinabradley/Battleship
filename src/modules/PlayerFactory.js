12f511a48f5e878ee62a7403131398a6628f6dfd
takinabradley <bradfinney98@gmail.com>
2022-03-20 07:56:02 -0400

    created PlayerFactory module and tests

--- src/modules/PlayerFactory.js
+++ src/modules/PlayerFactory.js
@@ -1,23 +1,23 @@
 import BoardFactory from "./BoardFactory"
 
-export default function PlayerFactory(name) {
+export default function PlayerFactory(name, isComputer = false) {
+  if (typeof name !== "string") return
   // players take turns attacking the enemy gameboard
   // players might each have their own gameboard and ships
 
   // computer players should be able to make random, legal plays
 
-  /* 
-Player = {
-  name,
-  Gameboard
-}
-*/
   const gameboard = BoardFactory()
 
-  return {
+  return Object.freeze({
     get name() {
       return name
     },
-    gameboard,
-  }
+    get isComputer() {
+      return isComputer
+    },
+    get gameboard() {
+      return gameboard
+    },
+  })
 }
