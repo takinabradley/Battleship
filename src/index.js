9aed7753ea06524ea74aaa0bee0cb6d652be1f9d
takinabradley <bradfinney98@gmail.com>
2022-04-02 23:03:36 -0400

    cleaned up code comments

--- src/index.js
+++ src/index.js
@@ -2,17 +2,3 @@ import Game from "./modules/Game.js"
 import DOMController from "./modules/DOMController"
 
 Game.start()
-/*
-Game.player1 = PlayerFactory("player1")
-Game.player2 = PlayerFactory("player2")
-Game.startGame()
-console.log(Game.currentPlayer)
-*/
-/*
-document.addEventListener("startgame", DOMController.renderPlayerPage)
-document.addEventListener(
-  "playAgain",
-  DOMController.renderShipPage(Game.player1)
-)
-// Game.startGame()
-*/
